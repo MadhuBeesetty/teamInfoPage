83bb8c14c749977288ef018defde16d1b88b7949
MadhuBeesetty <madhusudhan@MacBook-Air.local>
2024-07-30 21:13:01 -0700

    restructure of folders

---
+++ src/Components/DisplayPage/DisplayPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const DisplayPage = () => {
+  return (
+    <div>
+      <header>
+        <p>
+          Display Page
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default DisplayPage;
